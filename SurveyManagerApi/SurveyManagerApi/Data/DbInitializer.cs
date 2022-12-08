684a4d0e3161a288b75bf9706eba645b36711f12
domcos223 <horvath.dominika223@gmail.com>
2022-12-08 23:06:22 +0100

    orderid added to question and option model

--- SurveyManagerApi/SurveyManagerApi/Data/DbInitializer.cs
+++ SurveyManagerApi/SurveyManagerApi/Data/DbInitializer.cs
@@ -12,8 +12,8 @@ namespace SurveyManagerApi.Data
             }
 
             var surveys = new List<Survey>();
-            var survey_dogs = new Survey { Id = "Survey1", Name = "Dogs Survey", Details = "Survey about your favorite dog breeds!", OptionType="1" };
-            var survey_cats = new Survey { Id = "Survey2", Name = "Cats Survey", Details = "Survey about your favorite cat breeds!", OptionType= "2"};
+            var survey_dogs = new Survey { Id = "Survey1", Name = "Dogs Survey", Details = "Survey about dogs!", OptionType = "1" };
+            var survey_cats = new Survey { Id = "Survey2", Name = "Cats Survey", Details = "Survey about cats!", OptionType = "2" };
 
             surveys.Add(survey_dogs);
             surveys.Add(survey_cats);
@@ -27,10 +27,13 @@ namespace SurveyManagerApi.Data
             {
                 return;   // DB has been seeded
             }
-            var questions = new List<Question>();
-
-            questions.Add(new Question { Id="Question1", SurveyId = "Survey1", Text = "What is your favorite dog breed?", MostAnsweredOp = "" });
-            questions.Add(new Question { Id = "Question2", SurveyId = "Survey1", Text = "Do you have a dog?", MostAnsweredOp = "" });
+            var questions = new List<Question>
+            {
+                new Question { Id = "Question1", OrderId = 1, SurveyId = "Survey1", Text = "Test Question 1", MostAnsweredOp = "" },
+                new Question { Id = "Question2", OrderId = 2, SurveyId = "Survey1", Text = "Test Question 2", MostAnsweredOp = "" },
+                new Question { Id = "Question3", OrderId = 1, SurveyId = "Survey2", Text = "Test Question 3", MostAnsweredOp = "" },
+                new Question { Id = "Question4", OrderId = 2, SurveyId = "Survey2", Text = "Test Question 4", MostAnsweredOp = "" }
+            };
 
             context.Questions.AddRange(questions);
             context.SaveChanges();
@@ -40,21 +43,29 @@ namespace SurveyManagerApi.Data
                 return; // DB has been seeded
             }
 
-          
-            var options = new List<Option>();
+
+            var options = new List<Option>
+            {
+                new Option { Id = "Question1_1", OrderId = 1, QuestionId = "Question1", Text = "Test Option 1" },
+                new Option { Id = "Question1_2", OrderId = 2, QuestionId = "Question1", Text = "Test Option 2" },
+                new Option { Id = "Question1_3", OrderId = 3, QuestionId = "Question1", Text = "Test Option 3" },
+
+                new Option { Id = "Question2_1", OrderId = 1, QuestionId = "Question2", Text = "Test Option 1" },
+                new Option { Id = "Question2_2", OrderId = 2, QuestionId = "Question2", Text = "Test Option 2" },
+
+                new Option { Id = "Question3_1", OrderId = 1, QuestionId = "Question3", Text = "Test Option 1" },
+                new Option { Id = "Question3_2", OrderId = 2, QuestionId = "Question3", Text = "Test Option 2" },
+                new Option { Id = "Question3_3", OrderId = 3, QuestionId = "Question3", Text = "Test Option 3" },
+
+                new Option { Id = "Question4_1", OrderId = 1, QuestionId = "Question4", Text = "Test Option 1" },
+                new Option { Id = "Question4_2", OrderId = 2, QuestionId = "Question4", Text = "Test Option 2" },
+                new Option { Id = "Question4_3", OrderId = 3, QuestionId = "Question4", Text = "Test Option 3" },
+            };
 
 
-            options.Add(new Option { Id="Question1_1", QuestionId = "Question1", Text = "Husky"});
-            options.Add(new Option { Id = "Question1_2", QuestionId = "Question1", Text = "Golden Retriever" });
-            options.Add( new Option { Id = "Question1_3", QuestionId = "Question1", Text = "Airdale Terrier" });
-
-            options.Add(new Option { Id = "Question2_1", QuestionId = "Question2", Text = "Yes" });
-            options.Add(new Option { Id = "Question2_2", QuestionId = "Question2", Text = "No" });
-
-           
             context.Options.AddRange(options);
             context.SaveChanges();
-            
+
         }
     }
 }
---
+++ SurveyManagerApi/SurveyManagerApi/Migrations/20221208215736_UpdateModelOrderId.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SurveyManagerApi.Migrations
+{
+    public partial class UpdateModelOrderId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OrderId",
+                table: "Questions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "OrderId",
+                table: "Options",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrderId",
+                table: "Questions");
+
+            migrationBuilder.DropColumn(
+                name: "OrderId",
+                table: "Options");
+        }
+    }
+}
